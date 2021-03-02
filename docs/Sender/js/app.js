7cb49a222a647bd3288d0546df6b5e75ad33b657
Romain Talou <32712553+ComputerGraphicClub@users.noreply.github.com>
2021-03-02 15:24:05 +0100

    Add files via upload

--- docs/Sender/js/app.js
+++ docs/Sender/js/app.js
@@ -56,7 +56,20 @@ function startRecording() {
 		audioContext = new AudioContext();
 
 		//update the format
-		document.getElementById("formats").innerHTML="Recording Process.. Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+		document.getElementById("recordStatue").innerHTML="Recording on channel 1 at pcm @ "+audioContext.sampleRate/1000+"kHz";
+
+		//document.getElementById("recordStatue").style.animationName = "color2";
+
+		document.getElementById("subcursor0").style.color = "rgba(0, 0, 0, 0.3)";
+
+		document.getElementById("subcursor").style.color = "rgba(0, 0, 0, 0.3)";
+
+		//document.getElementById("recordStatue").style.color = "rgba(255, 0, 0, 0.7)";
+
+
+
+
+
 
 		/*  assign to gumStream for later use  */
 		gumStream = stream;
@@ -122,7 +135,7 @@ function stopRecording() {
 	rec.exportWAV(createDownloadLink);
 
 	//update the format
-	document.getElementById("formats").innerHTML="Send us your message and hope to see you soon..."
+	document.getElementById("formats").innerHTML="Send us your message & hope to see you soon..."
 
 }
 
@@ -168,7 +181,7 @@ function createDownloadLink(blob) {
 		  };
 		  var fd=new FormData();
 		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","https://projects.mastermediadesign.ch/Sender/upload.php",true);
+		  xhr.open("POST","upload.php",true);
 		  xhr.send(fd);
 	})
 	li.appendChild(document.createTextNode (" "))//add a space in between
@@ -177,3 +190,100 @@ function createDownloadLink(blob) {
 	//add the li element to the ol
 	recordingsList.appendChild(li);
 }
+
+//typewritter
+
+setTimeout(function(){
+
+// values to keep track of the number of letters typed, which quote to use. etc. Don't change these values.
+var i = 0,
+    a = 0,
+    isBackspacing = false,
+    isParagraph = false;
+
+// Typerwrite text content. Use a pipe to indicate the start of the second line "|".
+var textArray = [
+  "What is your last memory of being at your workspace?|Thank you."
+];
+
+// Speed (in milliseconds) of typing.
+var speedForward = 40, //Typing Speed
+    speedWait = 2000, // Wait between typing and backspacing
+    speedBetweenLines = 800, //Wait between first and second lines
+    speedBackspace = 25; //Backspace Speed
+
+//Run the loop
+typeWriter("output", textArray);
+
+
+
+function typeWriter(id, ar) {
+  var element = $("#" + id),
+      aString = ar[a],
+      eHeader = element.children("h3"), //Header element
+      eParagraph = element.children("h4"); //Subheader element
+
+  // Determine if animation should be typing or backspacing
+  if (!isBackspacing) {
+
+    // If full string hasn't yet been typed out, continue typing
+    if (i < aString.length) {
+
+      // If character about to be typed is a pipe, switch to second line and continue.
+      if (aString.charAt(i) == "|") {
+        isParagraph = true;
+        eHeader.removeClass("cursor");
+        eParagraph.addClass("cursor");
+        i++;
+        setTimeout(function(){ typeWriter(id, ar); }, speedBetweenLines);
+
+      // If character isn't a pipe, continue typing.
+      } else {
+        // Type header or subheader depending on whether pipe has been detected
+        if (!isParagraph) {
+          eHeader.text(eHeader.text() + aString.charAt(i));
+        } else {
+          eParagraph.text(eParagraph.text() + aString.charAt(i));
+        }
+        i++;
+        setTimeout(function(){ typeWriter(id, ar); }, speedForward);
+      }
+
+    // If full string has been typed, switch to backspace mode.
+    } else if (i == aString.length) {
+
+      isBackspacing = true;
+      setTimeout(function(){ typeWriter(id, ar); }, speedWait);
+
+    }
+
+  // If backspacing is enabled
+  } else {
+
+    // If either the header or the paragraph still has text, continue backspacing
+    if (eHeader.text().length > 0 || eParagraph.text().length > 0) {
+
+      // If paragraph still has text, continue erasing, otherwise switch to the header.
+      if (eParagraph.text().length > 0) {
+        eParagraph.text(eParagraph.text().substring(0, eParagraph.text().length - 1));
+      } else if (eHeader.text().length > 0) {
+        eParagraph.removeClass("cursor");
+        eHeader.addClass("cursor");
+        eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));
+      }
+      setTimeout(function(){ typeWriter(id, ar); }, speedBackspace);
+
+    // If neither head or paragraph still has text, switch to next quote in array and start typing.
+    } else {
+
+      isBackspacing = false;
+      i = 0;
+      isParagraph = false;
+      a = (a + 1) % ar.length; //Moves to next position in array, always looping back to 0
+      setTimeout(function(){ typeWriter(id, ar); }, 50);
+
+    }
+  }
+}
+
+}, 7500)
